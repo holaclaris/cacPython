fac1be27ef3028a6902c949a56e6d972f4e22562
Hugo Llaguno <124548588+LlagHE@users.noreply.github.com>
2024-07-11 02:06:19 -0300

    Update mensaje Flash

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, flash, render_template, request, redirect
 from controller import *
 
 app = Flask(__name__)
+app.secret_key = 'Grupo17'
 
 #Rutas
 @app.route('/')
@@ -15,39 +16,51 @@ def destinos():
     return render_template('destinos.html', paquetes=paquetes)
 
 # Create
-@app.route("/destinos/guardar_nuevo_paquete", methods=["POST"])
+@app.route('/destinos/guardar_nuevo_paquete', methods=['POST'])
 def guardar_nuevo_paquete_route():
-    nom = request.form["nombre"]
-    dest = request.form["destino"]
-    desc = request.form["descripcion"]
-    imgURL = request.form["imagen"]
-    prec = request.form["precio"]
-    disp = request.form["disponible"]
+    nom = request.form['nombre']
+    dest = request.form['destino']
+    desc = request.form['descripcion']
+    imgURL = request.form['imagen']
+    prec = request.form['precio']
+    disp = request.form['disponible']
 
-    guardar_nuevo_paquete(nom, dest, desc, imgURL, prec, disp)
+    filas_agregadas = guardar_nuevo_paquete(nom, dest, desc, imgURL, prec, disp)
+    if filas_agregadas > 0:
+        flash('El paquete se agrego exitosamente!', 'success')
+    else:
+        flash('El paquete no pudo ser agregado!', 'error')
 
-    return redirect("/destinos")
+    return redirect('/destinos')
 
 # Update
-@app.route("/destinos/editar_paquete/<int:id>", methods=["POST"])
+@app.route('/destinos/editar_paquete/<int:id>', methods=['POST'])
 def editar_paquete_route(id):
-    nom = request.form["nombre"]
-    dest = request.form["destino"]
-    desc = request.form["descripcion"]
-    imgURL = request.form["imagen"]
-    prec = request.form["precio"]
-    disp = request.form["disponible"]
+    nom = request.form['nombre']
+    dest = request.form['destino']
+    desc = request.form['descripcion']
+    imgURL = request.form['imagen']
+    prec = request.form['precio']
+    disp = request.form['disponible']
 
-    editar_paquete(id, nom, dest, desc, imgURL, prec, disp)
+    filas_modificadas = editar_paquete(id, nom, dest, desc, imgURL, prec, disp)
+    if filas_modificadas > 0:
+        flash('El paquete se actualizo correctamente!', 'success')
+    else:
+        flash('El paquete no pudo ser actualizo!', 'error')
 
-    return redirect("/destinos")
+    return redirect('/destinos')
 
 
 # delete
 @app.route('/destinos/eliminar_paquete/<int:id>')
 def delete_paquete(id):
-    eliminar_paquete(id)
-    return redirect("/destinos")
+    filas_eliminadas = eliminar_paquete(id)
+    if filas_eliminadas > 0:
+        flash('El paquete se elimino correctamente!', 'success')
+    else:
+        flash('El paquete no pudo ser eliminado!', 'error')
+    return redirect('/destinos')
 
 @app.route('/guias')
 def guias():
