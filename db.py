404b1498bbe777d266e5495dedfc8af7e1608453
holaclaris <mamutchiquitito@gmail.com>
2024-07-11 22:27:40 -0300

    cambios db para deploy e imagen que faltaba

--- db.py
+++ db.py
@@ -3,10 +3,10 @@
 
 import pymysql
 
-host = "SofiaBaldera369.mysql.pythonanywhere-services.com"
-user = "SofiaBaldera369"
-password = "wawviajes"
-db = "SofiaBaldera369$predeterminado"
+host = "holaclaris.mysql.pythonanywhere-services.com"
+user = "holaclaris"
+password = "lunita29"
+db = "holaclaris$waw"
 
 def conexionMySQL():
     return pymysql.connect(host=host, user=user, password=password, database=db)
