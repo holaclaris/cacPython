0704eaf5bcf2170006370116458a75d5021c37c2
holaclaris <mamutchiquitito@gmail.com>
2024-07-11 23:35:09 -0300

    cambios de prueba db

--- db.py
+++ db.py
@@ -3,10 +3,14 @@
 
 import pymysql
 
-host = "holaclaris.mysql.pythonanywhere-services.com"
-user = "holaclaris"
-password = "lunita29"
-db = "holaclaris$waw"
+host = "localhost"
+user = "root"
+password = ""
+db = "wawdb"
+# host = "holaclaris.mysql.pythonanywhere-services.com"
+# user = "holaclaris"
+# password = "lunita29"
+# db = "holaclaris$waw"
 
 def conexionMySQL():
     return pymysql.connect(host=host, user=user, password=password, database=db)
