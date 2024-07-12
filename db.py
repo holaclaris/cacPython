fdc25c65f18e306cb5b167d2bb14d77112af58c3
holaclaris <mamutchiquitito@gmail.com>
2024-07-11 23:59:56 -0300

    agregada nueva db

--- db.py
+++ db.py
@@ -13,7 +13,7 @@ db = "wawdb"
 # host = "holaclaris.mysql.pythonanywhere-services.com"
 # user = "holaclaris"
 # password = "lunita29"
-# db = "holaclaris$waw"
+# db = "holaclaris$wawdatabase"
 
 def conexionMySQL():
     return pymysql.connect(host=host, user=user, password=password, database=db)
